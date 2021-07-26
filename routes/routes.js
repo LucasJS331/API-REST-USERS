d8a6cd6b23c730f823dc615415c80c858c2a1356
LucasJS331 <69942512+LucasJS331@users.noreply.github.com>
2021-07-25 23:56:15 -0300

    fix user routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,16 +4,11 @@ const UserController = require("../controllers/UserController")
 const auth = require("../middleware/admin-midleware");
 
 //user routes
-
 router.post("/user",auth, UserController.create);
 router.get("/users", auth, UserController.findAll);
 router.get("/user/:id", auth, UserController.findOne);
 router.put("/user/:id", auth, UserController.edit);
-router.delete("/user/:id", auth, UserController.remove);
-router.post("/recover", auth, UserController.recoverPassword);
-router.post("/changePassword", UserController.changePassword);
+router.delete("/user/:id",auth, UserController.remove);
 router.post("/auth", UserController.login);
-
-
 
 module.exports = router;
